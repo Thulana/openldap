64370bef8d7ed3dec47c2006a69a7277db4d8eee
thulana <thulanakannangara@gmail.com>
2017-08-28 12:43:42 +0530

    restart ldap service before test

--- tests/unit/OpenLdapContainerCest.php
+++ tests/unit/OpenLdapContainerCest.php
@@ -21,6 +21,7 @@ class OpenLdapContainerCest
 
     public function checkSlapdService(UnitTester $I){
         $I->wantTo("verify slapd is configured in the container");
+        $I->runShellCommand("docker exec openldap service slapd restart");
         $I->runShellCommand("docker exec openldap service slapd status");
         $I->seeInShellOutput("slapd is running.");
     }
